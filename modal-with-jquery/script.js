d619c8aaa28aee893930babf0475bbe895e57680
itsumoonazicode <dyingkouki@gmail.com>
2021-08-08 01:43:50 +0900

    背景部分をクリックしたときのみモーダルを非表示にする #1

--- modal-with-jquery/script.js
+++ modal-with-jquery/script.js
@@ -1,6 +1,14 @@
 $(function() {
   $('#modal-btn').on('click', function() {
     $('.modal').addClass('modal-container');
+    $('.modal-container').on('click', function(event) {
+      // 背景部分をクリックしたときのみモーダルを非表示にする
+      if(event.target !== event.currentTarget) {
+        return;
+      } else {
+        $('.modal').removeClass('modal-container');
+      }
+    })
   });
   $('.modal-close').on('click', function() {
     $('.modal').removeClass('modal-container');
